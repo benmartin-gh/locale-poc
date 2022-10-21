823df0638535b357acb9f1389f4ca90a5a3fc116
Ben Martin <ben@martin.com>
2022-10-21 13:38:03 +1100

    add next-i18next

---
+++ next-i18next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  i18n: {
+    locales: ["en-AU", "en-NZ", "en"],
+    defaultLocale: "en",
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,41 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
+import { useTranslation, Trans } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const { t } = useTranslation("common");
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{t("title")}</title>
         <meta name="description" content="Locale poc app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>Your local is {router.locale}</h1>
-        {router.locale === router.defaultLocale ? <p>DEFAULT</p> : null}
+        <h1>
+          Your locale is {router.locale}{" "}
+          {router.locale === router.defaultLocale ? "(default)" : null}
+        </h1>
+
+        <h3>{t("greeting")}</h3>
       </main>
     </div>
   );
 };
+
+export async function getStaticProps(props: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(props.locale, ["common"])),
+    },
+  };
+}
 
 export default Home;
